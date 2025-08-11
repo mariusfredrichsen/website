6ea628ff86449cf360884af76b70b62033db8aac
Bsian02 <mariusfredrichsen@gmail.com>
2025-08-11 02:32:13 +0200

    change to grid format in cv page to take advantage of wide screens

--- src/pages/CV/CV.tsx
+++ src/pages/CV/CV.tsx
@@ -98,7 +98,7 @@ function CV() {
     const cvContent = cvContentJson as CVContent;
 
     return (
-        <div className="flex flex-col items-center pt-32 gap-8">
+        <div className="items-center pt-32 gap-8 p-4">
             <header className="flex flex-col items-center w-[min(90vw,600px)] mx-auto">
                 <h1 className="mb-16 text-center ">Marius Fredrichsen</h1>
                 <div className="mb-16 text-left">
@@ -109,117 +109,136 @@ function CV() {
                 </div>
             </header>
 
-            <div className="card-1">
-                <header>
-                    <h2>Erfaringer</h2>
-                </header>
-                <hr />
-                <div className="flex flex-col gap-8">
-                    {
-                        cvContent.experience.map((it, index) => {
-                            return (
-                                <div key={index}>
-                                    <p><strong>{it.title}</strong></p>
-                                    <p className="date">{it.date_from} - {it.date_to}</p>
-                                    <div>
-                                        {parseDescription(it.description)}
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </div>
 
-            <div className="card-1">
-                <header>
-                    <h2>Utdanning</h2>
-                </header>
-                <hr />
-                <div className="flex flex-col gap-8">
-                    {
-                        cvContent.education.map((it1, index1) => {
-                            return (
-                                <div key={index1} className="flex flex-col divide-gray-500">
-                                    <header>
-                                        <h4><strong>{it1.title_of_education}</strong></h4>
-                                        <p className="date">{it1.date_from} - {it1.date_to}</p>
-                                        <p>{it1.educational_institution}</p>
-                                    </header>
-                                    {
-                                        it1.semesters.map((it2, index2) => {
-                                            return (
-                                                <>
-                                                    <hr className="my-4" />
-                                                    <div key={index2}>
-                                                        <div>
-                                                            <header>
-                                                                <h5>{it2.number}. Semester {(index1 === 0 && index2 === 0) ? "(while true):" : ""}</h5>
-                                                            </header>
-                                                            {parseDescription(it2.courses)}
+            <div className="container-layout">
+
+                <div className="card-1 projects">
+                    <header>
+                        <h2>Prosjekter</h2>
+                    </header>
+                    <hr />
+                    <div className="projects-layout">
+                        <div className="projects-list">
+                            {
+                                cvContent.projects.map((it1, index) => {
+                                    return (
+                                        <div key={index} className="project">
+                                            <header>
+                                                <h4><strong>{it1.title}</strong></h4>
+                                                <p className="date">Teknologier: {it1.technologies.map((it2) => it2.name).join(", ")}</p>
+                                            </header>
+                                            <div>
+                                                {parseDescription(it1.description)}
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className="technologies">
+                            <h4>Teknologier</h4>
+                            <ul className="technologies-list">
+                                {
+                                    Array.from(
+                                        new Set(
+                                            cvContent.projects.flatMap((it) => it.technologies.map((tech) => tech.name))
+                                        )
+                                    ).map((it) => <li>{it}</li>)
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card-1 experience">
+                    <header>
+                        <h2>Erfaringer</h2>
+                    </header>
+                    <hr />
+                    <div className="flex flex-col gap-8">
+                        {
+                            cvContent.experience.map((it, index) => {
+                                return (
+                                    <div key={index}>
+                                        <p><strong>{it.title}</strong></p>
+                                        <p className="date">{it.date_from} - {it.date_to}</p>
+                                        <div>
+                                            {parseDescription(it.description)}
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+
+                <div className="card-1 education">
+                    <header>
+                        <h2>Utdanning</h2>
+                    </header>
+                    <hr />
+                    <div className="flex flex-col gap-8">
+                        {
+                            cvContent.education.map((it1, index1) => {
+                                return (
+                                    <div key={index1} className="flex flex-col divide-gray-500">
+                                        <header>
+                                            <h4><strong>{it1.title_of_education}</strong></h4>
+                                            <p className="date">{it1.date_from} - {it1.date_to}</p>
+                                            <p>{it1.educational_institution}</p>
+                                        </header>
+                                        {
+                                            it1.semesters.map((it2, index2) => {
+                                                return (
+                                                    <>
+                                                        <hr className="my-4" />
+                                                        <div key={index2}>
+                                                            <div>
+                                                                <header>
+                                                                    <h5>{it2.number}. Semester {(index1 === 0 && index2 === 0) ? "(while true):" : ""}</h5>
+                                                                </header>
+                                                                {parseDescription(it2.courses)}
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                </>
-                                            )
-                                        })
-                                    }
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </div>
-
-            <div className="card-1">
-                <header>
-                    <h2>Prosjekter</h2>
-                </header>
-                <hr />
-                <div className="flex flex-col gap-8">
-                    {
-                        cvContent.projects.map((it1, index) => {
-                            return (
-                                <div key={index}>
-                                    <header>
-                                        <h4><strong>{it1.title}</strong></h4>
-                                        <p className="date">Teknologier: {it1.technologies.map((it2) => it2.name).join(", ")}</p>
-                                    </header>
-                                    <div>
-                                        {parseDescription(it1.description)}
+                                                    </>
+                                                )
+                                            })
+                                        }
                                     </div>
-                                </div>
-                            )
-                        })
-                    }
+                                )
+                            })
+                        }
+                    </div>
                 </div>
-            </div>
 
-            <div className="card-1">
-                <header>
-                    <h2>Annet</h2>
-                </header>
-                <hr />
-                <div className="flex flex-col">
-                    {
-                        cvContent.other.map((it1, index) => {
-                            return (
-                                <p key={index}>{it1.title} - {it1.description}</p>
-                            )
-                        })
-                    }
+                <div className="card-1 other">
+                    <header>
+                        <h2>Annet</h2>
+                    </header>
+                    <hr />
+                    <div className="flex flex-col">
+                        {
+                            cvContent.other.map((it1, index) => {
+                                return (
+                                    <p key={index}>{it1.title} - {it1.description}</p>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
-            </div>
 
-            <div className="card-1">
-                <header>
-                    <h2>Referanser</h2>
-                </header>
-                <hr />
-                <div className="flex flex-col">
-                    <p>{cvContent.referances[0].title}</p>
+                <div className="card-1 references">
+                    <header>
+                        <h2>Referanser</h2>
+                    </header>
+                    <hr />
+                    <div className="flex flex-col">
+                        <p>{cvContent.referances[0].title}</p>
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
